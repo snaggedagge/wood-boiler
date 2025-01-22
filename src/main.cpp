d25a2152e2e78bb1956c7b8205e0edbff87db913
Dag Karlsson <dag-karlsson@hotmail.com>
2025-01-22 22:02:26 +0200

    Reset last commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Wire.h>
 #include <max6675.h>
 
-
 #include "Display.h"
 #include "Timer.h"
 #include "AirDamper.h"
@@ -25,12 +24,11 @@
 #define SCK_PIN 15 // D8
 #define CS_PIN 13 // D7
 
-
 #define DIRECTION_PIN 3 // RX
 #define STEP_PIN 1 // TX
 #define STEPPER_SLEEP_PIN 14 // D5
 
-Stats stats = {0, 0, 0, 100, 250, true, 0};
+
 
 // PID parameters
 double Kp = 0.018;   // Proportional gain
@@ -45,13 +43,15 @@ MAX6675 waterSensor(SCK_PIN, CS_PIN, SO_PIN_WATER);
 MAX6675 exhaustSensor(SCK_PIN, CS_PIN, SO_PIN_EXHAUST);
 AirDamper primaryAirDamper(DIRECTION_PIN, STEP_PIN, STEPPER_SLEEP_PIN, 56, logManager);
 Timer timer;
-WebserverConfiguration config("4G-Gateway-21E0", "snaggedagge", logManager, stats);
+Stats stats = {0, 0, 0, 100, 250, true, 0};
+WebserverConfiguration webserverConfig("4G-Gateway-21E0", "snaggedagge", logManager, stats);
 
 int wantedTemperature = 185;
 bool reachedTemperature = false;
 
 float readSensor(MAX6675& sensor, int lowerLimit, int higherLimit) {
   int counter = 0;
+  int errorCounter = 0;
   float accumulatedTemperature = 0;
 
   while (counter < 3) { // Take average of three readings
@@ -60,6 +60,13 @@ float readSensor(MAX6675& sensor, int lowerLimit, int higherLimit) {
     {
       accumulatedTemperature += temperature;
       counter++;
+    }
+    else {
+      errorCounter++;
+      if (errorCounter > 3)
+      {
+        return higherLimit;
+      }
     }
     delay(50);
   }
@@ -82,19 +89,19 @@ void setup() {
   primaryAirDamper.init();
   primaryAirDamper.hardResetPosition();
   primaryAirDamper.moveToStep(20);
-  config.init();
+  webserverConfig.init();
 }
 
 void loop() {
-  config.handleUpdate();  
+  webserverConfig.handleUpdate();  
   unsigned long millisSinceStart = millis();
   int sinceStartedMinutes = millisSinceStart / 1000 / 60;
   stats.heating = !(sinceStartedMinutes > 60 && stats.exhaustTemperature < stats.lowerExhaustLimit) && stats.waterTemperature < 100;
   stats.burnTimeMinutes = stats.heating ? sinceStartedMinutes : stats.burnTimeMinutes;
 
   // Let temperature get high enough before PID takes over, difficult to use same config in starting phase as in burning phase.
-  // using different gains could also work
-  if (!reachedTemperature && stats.exhaustTemperature > 160)
+  // In case it is hot when starting, let PID take over immediately. Probably reboot or power outage
+  if ((!reachedTemperature && stats.exhaustTemperature > 160) || (!reachedTemperature && stats.exhaustTemperature > 100 && sinceStartedMinutes == 0))
   {
     reachedTemperature = true;
     primaryAirDamper.moveToStep(11);
